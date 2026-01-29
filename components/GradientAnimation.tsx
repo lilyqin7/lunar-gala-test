02c510bb1dd79b4fb1c54ca048e103c79bd534ee
lilyqin7 <lilyqin527@gmail.com>
2026-01-29 02:13:22 -0500

    add grids

--- components/GradientAnimation.tsx
+++ components/GradientAnimation.tsx
@@ -30,25 +30,189 @@ export default function GradientAnimation() {
       />
       
       {/* Grid of squares */}
-      <div className="relative z-10 grid grid-cols-5 gap-4 h-full p-4">
-        {/* Empty squares - background shows through */}
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
-        <div className="border border-black bg-transparent" />
+      <div className="relative w-full h-screen overflow-hidden">
+        <div className="row24px">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row24px">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row48px">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row-eighth">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+        </div>
+        <div className="row-quarter">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row-half">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+        </div>
+        <div className="row-full">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+        </div>
+        <div className="row-half">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+        </div>
+        <div className="row-quarter">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row-eighth">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row48px">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+        </div>
+        <div className="row24px">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
+        <div className="row24px">
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v3"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-full gradient-vertical" id="gradient-animation-v2"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v4"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-half gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+          <div className="rectangle-small gradient-vertical" id="gradient-animation-v1"> </div>
+        </div>
       </div>
     </div>
   );
